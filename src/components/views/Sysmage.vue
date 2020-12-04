d2eec7c997beb40e0ee2d31e35f374b61d541156
liuxiaoyu08 <liuxiaoyu08@meicai.cn>
2020-12-04 19:15:30 +0800

    add

--- src/components/views/Sysmage.vue
+++ src/components/views/Sysmage.vue
@@ -20,7 +20,7 @@
                 <el-button type="primary" icon="el-icon-search" @click="handleSearch">搜索</el-button>
             </div>
             <el-table
-                :data="tableData"        border
+                :data="tableData"
                 class="table"
                 ref="multipleTable"
                 header-cell-class-name="table-header"
